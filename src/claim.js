73109632502e666036354f69359bece6904c6fe7
peter4885 <ushijima242@gmail.com>
2024-02-15 19:12:25 +0900

    add info of contract

--- src/claim.js
+++ src/claim.js
@@ -1,1 +1,33 @@
-console.log(Snake.getFoodEaten());
+const Web3 = require('web3');
+const WalletConnectProvider = require('@walletconnect/web3-provider');
+
+const contractABI = ;
+const contractAddress = ;
+
+
+const provider = new WalletConnectProvider({
+  rpc: {
+    168587773: 'https://sepolia.blast.io',
+
+  },
+});
+
+const web3 = new Web3(provider);
+
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+async function claimTokens(score) {
+  try {
+    const accounts = await web3.eth.getAccounts();
+    const userAddress = accounts[0];
+
+    const result = await contract.methods.claimTokens(score).send({ from: userAddress });
+
+    console.log('Tokens claimed successfully:', result);
+  } catch (error) {
+    console.error('Error claiming tokens:', error);
+  }
+}
+
+const scoreToClaim = 100;
+claimTokens(scoreToClaim);
